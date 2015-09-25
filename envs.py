60d22f2c4025ae115ffdb57e0b465bd92e29329c
Patrick Golden <ptgolden0@gmail.com>
2015-09-25 16:26:36 -0400

    Update envs file one more time

--- envs.py
+++ envs.py
@@ -4,26 +4,36 @@ Deployment environments
 
 from fabric.api import env, task
 
-@task
-def working_notes_test():
-    "Use the beta-testing webserver."
-    env.project_name = 'workingnotes-test'
-    env.hosts = ['test.workingnotes.org']
+def make_basic_conf(hostname):
+    "Add the basic env values for our server"
+    env.hosts = [hostname]
 
-    env.project_path = '/usr/local/projects/{project_name}'.format(**env)
+    env.project_path = '/usr/local/projects/{}'.format(hostname)
     env.python = '/usr/bin/python2.7'
 
-    env.uwsgi_conf_file = '/etc/uwsgi/sites/{project_name}.ini'.format(**env)
+    env.uwsgi_conf_file = '/etc/uwsgi/sites/{}.ini'.format(hostname)
     env.uwsgi_uid = 'ryanshaw'
     env.uwsgi_gid = 'ryanshaw'
-    env.uwsgi_socket_location = '/run/uwsgi/{project_name}.sock'.format(**env)
+    env.uwsgi_socket_location = '/run/uwsgi/{}.sock'.format(hostname)
     env.uwsgi_socket_uid = 'www-data'
     env.uwsgi_socket_gid = 'www-data'
     env.uwsgi_socket_chmod = 644
 
+    env.nginx_conf_file = '/etc/nginx/conf.d/{}.conf'.format(hostname)
+
+@task
+def working_notes_test():
+    "Use the main Working Notes host."
+    hostname = 'test.workingnotes.org'
+    make_basic_conf(hostname)
     env.renderer_port = 15023
 
-    env.nginx_conf_file = '/etc/nginx/conf.d/{project_name}.conf'
+@task
+def working_notes():
+    "Use the testing Working Notes host."
+    hostname = 'workingnotes.org'
+    make_basic_conf(hostname)
+    env.renderer_port = 15024
 
 @task
 def beta():
