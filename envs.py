bd6dd0b7d1cfb23d6c45d04d1da29280fb8a38fd
Patrick Golden <ptgolden0@gmail.com>
2015-09-25 16:03:41 -0400

    Update environment variables for envs

--- envs.py
+++ envs.py
@@ -10,15 +10,20 @@ def working_notes_test():
     env.project_name = 'workingnotes-test'
     env.hosts = ['test.workingnotes.org']
 
-    env.project_path = '/db/projects/{project_name}'.format(**env)
+    env.project_path = '/usr/local/projects/{project_name}'.format(**env)
     env.python = '/usr/bin/python2.7'
 
-    env.uwsgi_uid = 'patrick'
-    env.uwsgi_gid = 'patrick'
-    env.uwsgi_socket_location = '/run/uwsgi/{project_name}'.format(**env)
+    env.uwsgi_conf_file = '/etc/uwsgi/sites/{project_name}.ini'.format(**env)
+    env.uwsgi_uid = 'ryanshaw'
+    env.uwsgi_gid = 'ryanshaw'
+    env.uwsgi_socket_location = '/run/uwsgi/{project_name}.sock'.format(**env)
     env.uwsgi_socket_uid = 'www-data'
     env.uwsgi_socket_gid = 'www-data'
     env.uwsgi_socket_chmod = 644
+
+    env.renderer_port = 15023
+
+    env.nginx_conf_file = '/etc/nginx/conf.d/{project_name}.conf'
 
 @task
 def beta():
